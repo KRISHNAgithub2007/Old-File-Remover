30a6e4ceb0553102730b2cce204db0474a94247d
KRISHNAgithub2007 <69421609+KRISHNAgithub2007@users.noreply.github.com>
2021-12-15 14:06:18 +0530

    Add files via upload

---
+++ deleteOldFiles.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import time
+
+path = input("Enter the path of the directory you want to clean up : ")
+current_time = time.time()
+daysToDeleteFrom = int(input("Number of days you want to delete from : "))
+
+if os.path.exists(path):
+    
+    listOfFiles = os.listdir(path)
+    for i in listOfFiles:
+        file_time = os.stat(path+'/'+i).st_mtime
+        days_difference = ((current_time - file_time) / 86400)
+        if days_difference >= daysToDeleteFrom:
+            os.remove(path+'/'+i)
+            print("Removed : "+i)
+else : 
+    print("Error : The path you specified was not found")
